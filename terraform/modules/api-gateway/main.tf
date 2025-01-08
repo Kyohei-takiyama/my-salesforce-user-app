da8f5b0c89bda44a89e628e130920bd21ced7e79
Kyohei-takiyama <takiyamakyohei@gmail.com>
2025-01-08 12:39:03 +0900

    API GatewayのCORS設定を追加し、POSTおよびOPTIONSメソッドのサポートを実装

--- terraform/modules/api-gateway/main.tf
+++ terraform/modules/api-gateway/main.tf
@@ -42,31 +42,109 @@ resource "aws_api_gateway_rest_api" "api" {
     }
     paths = {
       "/import" = {
+        # POST (Lambda Proxy)
         post = {
           x-amazon-apigateway-integration = {
-            httpMethod           = "POST" # LambdaへのアクセスはPOSTでないといけないらしい
+            httpMethod           = "POST"
             payloadFormatVersion = "1.0"
             type                 = "AWS_PROXY"
             uri                  = var.lambda_invoke_arn
             credentials          = aws_iam_role.api_gateway_role.arn
           }
         }
-      }
-
+        # OPTIONS (Mock)
+        options = {
+          x-amazon-apigateway-integration = {
+            type = "mock"
+            requestTemplates = {
+              "application/json" = "{\"statusCode\": 200}"
+            }
+            responses = {
+              "default" = {
+                "statusCode" = "200"
+                "responseParameters" = {
+                  "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+                  "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+                  "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+                }
+                "responseTemplates" = {
+                  "application/json" = ""
+                }
+              }
+            }
+          }
+          responses = {
+            "200" = {
+              description = "Default response for CORS"
+              headers = {
+                "Access-Control-Allow-Headers" = {
+                  "schema" = { "type" = "string" }
+                }
+                "Access-Control-Allow-Methods" = {
+                  "schema" = { "type" = "string" }
+                }
+                "Access-Control-Allow-Origin" = {
+                  "schema" = { "type" = "string" }
+                }
+              }
+            }
+          }
+        }
+      },
       "/create" = {
+        # POST (Lambda Proxy)
         post = {
           x-amazon-apigateway-integration = {
-            httpMethod           = "POST" # LambdaへのアクセスはPOSTでないといけないらしい
+            httpMethod           = "POST"
             payloadFormatVersion = "1.0"
             type                 = "AWS_PROXY"
             uri                  = var.lambda_invoke_arn
             credentials          = aws_iam_role.api_gateway_role.arn
+          }
+        }
+        # OPTIONS (Mock)
+        options = {
+          x-amazon-apigateway-integration = {
+            type = "mock"
+            requestTemplates = {
+              "application/json" = "{\"statusCode\": 200}"
+            }
+            responses = {
+              "default" = {
+                "statusCode" = "200"
+                "responseParameters" = {
+                  "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+                  "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+                  "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+                }
+                "responseTemplates" = {
+                  "application/json" = ""
+                }
+              }
+            }
+          }
+          responses = {
+            "200" = {
+              description = "Default response for CORS"
+              headers = {
+                "Access-Control-Allow-Headers" = {
+                  "schema" = { "type" = "string" }
+                }
+                "Access-Control-Allow-Methods" = {
+                  "schema" = { "type" = "string" }
+                }
+                "Access-Control-Allow-Origin" = {
+                  "schema" = { "type" = "string" }
+                }
+              }
+            }
           }
         }
       }
     }
   })
 }
+
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
